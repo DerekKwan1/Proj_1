5779efef2d4f4ccec85ea13a504093b2557935ca
Derek Kwan <33869595+DerekKwan1@users.noreply.github.com>
2019-11-04 16:20:49 -0500

    Add files via upload

---
+++ Project 1.R
@@ -0,0 +1,6 @@
+
+rnam= c("Zombies", "Dracula")
+cnam= c("M&M's","KitKat","Skittles","Snickers","Twix","3 Musketeers")
+title=list(rnam,cnam)
+trick<- matrix(c(1,2,3,4,5,6,7,8,9,10,11,12), nrow=2, ncol=6, dimnames = title)
+trick
